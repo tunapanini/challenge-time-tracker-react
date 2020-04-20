0d16f5aa4645472881b2c61cbe15fa1878a4375c
Euni Han <haneuneee@gmail.com>
2020-04-20 18:04:52 +0900

    Add @storybook/addon-console

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import { addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
+import '@storybook/addon-console';  // register @storybook/addon-console
+
 addDecorator(withKnobs);
 
 addParameters({
--- src/index.stories.js
+++ src/index.stories.js
@@ -5,12 +5,22 @@ import { action } from '@storybook/addon-actions';
 
 export default { title: 'Button' };
 
-export const withText = () => <Button onClick={action('button-click')}>{text('Text', 'Hello Button')}</Button>;
-
 export const withEmoji = () => (
     <Button onClick={action('button-click')}>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
     </Button>
+);
+
+export const withTextKnob = () => <Button>{text('Text', 'Hello Button')}</Button>;
+
+export const withAction = () => <Button onClick={action('button-click')}>Click Me</Button>;
+
+export const withConsole = () => (
+    <div>
+        <Button onClick={() => console.log('log')}>console.log</Button>
+        <Button onClick={() => console.warn('warn')}>console.warn</Button>
+        <Button onClick={() => console.error('error')}>console.error</Button>
+    </div>
 );

625539f65aeb8d8cdaba0723c535019253913838
Euni Han <haneuneee@gmail.com>
2020-04-20 17:34:14 +0900

    Add @storybook/addon-knobs

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Button } from '@storybook/react/demo';
+import { text } from '@storybook/addon-knobs';
 
 export default { title: 'Button' };
 
-export const withText = () => <Button>Hello Button</Button>;
+export const withText = () => <Button>{text('Text', 'Hello Button')}</Button>;
 
 export const withEmoji = () => (
     <Button>

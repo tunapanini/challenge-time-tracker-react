cc74db008909f3e886acc855841230d4d996ba39
Euni Han <haneuneee@gmail.com>
2020-04-20 17:36:51 +0900

    Add @storybook/addon-action

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from '@storybook/react/demo';
 import { text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 export default { title: 'Button' };
 
-export const withText = () => <Button>{text('Text', 'Hello Button')}</Button>;
+export const withText = () => <Button onClick={action('button-click')}>{text('Text', 'Hello Button')}</Button>;
 
 export const withEmoji = () => (
-    <Button>
+    <Button onClick={action('button-click')}>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>

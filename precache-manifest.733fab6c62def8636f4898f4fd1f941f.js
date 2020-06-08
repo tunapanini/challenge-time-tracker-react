dfea39ca2b8d64ff2c92e8c9a5c7b929c3b81772
tunapanini <haneuneee@gmail.com>
2020-06-08 08:46:59 +0000

    Deploying to gh-pages from  @ 7d7a550fce1945240a4f0bad541909928d4cd58b 🚀

---
+++ precache-manifest.733fab6c62def8636f4898f4fd1f941f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5233450d0277acc0c6d44e19ac17bfd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6892c732d509b0cc27f1",
+    "url": "/static/css/main.1715eecb.chunk.css"
+  },
+  {
+    "revision": "cb9dd3c5388a2b10fc08",
+    "url": "/static/js/2.498cf70d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.498cf70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6892c732d509b0cc27f1",
+    "url": "/static/js/main.d5bd566c.chunk.js"
+  },
+  {
+    "revision": "5549ba184ee66d178c3c",
+    "url": "/static/js/runtime-main.4f43829d.js"
+  },
+  {
+    "revision": "95befcb507d35a4a32c5ca930662e3ce",
+    "url": "/static/media/arrow.95befcb5.svg"
+  },
+  {
+    "revision": "0a2975334af4b6a9b1eec53883f6de4f",
+    "url": "/static/media/avatar.0a297533.svg"
+  },
+  {
+    "revision": "cd02f2b0bbbba42ca937f320c3c8de4b",
+    "url": "/static/media/notification.cd02f2b0.svg"
+  }
+]);
